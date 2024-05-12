ad08cbdbbe39c484eaa6a79372563201dce5ef99
movi-centallo <filippo.gerbaudo@movicentallo.it>
2024-05-12 22:54:23 +0200

    feat: error implementation and backend filtering

--- src/scripts/cantone-finder.mjs
+++ src/scripts/cantone-finder.mjs
@@ -2,14 +2,27 @@ import * as turf from '@turf/turf';
 import * as topojson from 'topojson-client';
 import data from '../../public/cantoni/cantoni.json';
 import cantoniInfo from '../../public/cantoni/cantoni-info.json';
-const debug = false; 
 
 
+/*
+Filtri: 
+-Tipologie dati accettate: 'building' or 'street' [✓]
+
+
+Errori da scatenare: 
+
+-Risposta/Richiesta con paese diverso da Centallo [✓]
+-Via inesistente in Centallo [✓]
+-Coordinate GPS non hanno corrispondenza con un area (sebbene passino dal decodificatore di coordinate)
+
+
+
+*/
 async function createGeoapifyRequest(address) {
     try {
         const encodedAddress = encodeURIComponent(address);
         const apiKey = "06731d7c24044a51a73ad31c7f3589bc";
-        const apiUrl = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&format=json&apiKey=${apiKey}`;
+        const apiUrl = `https://api.geoapify.com/v1/geocode/search?text=${encodedAddress}&postcode=12044&limit=3&format=json&apiKey=${apiKey}`;
         
         const response = await fetch(apiUrl);
         if (!response.ok) {
@@ -57,15 +70,27 @@ export async function cantoneFinder(address) {
         console.log('Script started'); 
         
         var geoapifyResponse = await createGeoapifyRequest(address);
-        console.log(geoapifyResponse); 
-       
-        const response = geoapifyResponse.results.filter(e => {
+        console.log(geoapifyResponse.results);
+        console.log(typeof geoapifyResponse.results);
+
+        const response = geoapifyResponse.results.filter((e) => {
             //Filter rules
-            return e.city == 'Centallo' && e.result_type == 'building'; 
-        })
+            return (e.result_type === "building" || e.result_type ===  "street" || e.result_type === "postcode"); 
+        });
+        
+        console.log("Risposta");
+        console.log(response); 
+        
+        if(response.length == 0){
+            const err_return = addInfo("not_found", true);
+            console.log(err_return); 
+            return err_return;
+            
+        }
+
 
         // Example usage:
-        const coordinate = [geoapifyResponse.results[0].lon, geoapifyResponse.results[0].lat];
+        const coordinate = [response[0].lon, response[0].lat];
         console.log(data);
         // Fetch the TopoJSON file
         //const topoJSONResponse = fetch('/cantoni/cantoni.topojson?url').then((response) => response.json()).then((json) => console.log(json));
@@ -79,23 +104,36 @@ export async function cantoneFinder(address) {
 
         // Find the area name for the coordinate
         const areaName = await getAreaNameForCoordinate(coordinate, data);
-        const cantoneInfo = addInfo(areaName); 
-        console.log(cantoneInfo);
+        
+        console.log(areaName);
+        const cantoneInfo = addInfo(areaName, false); 
+        
         return cantoneInfo;
+
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
 
-function addInfo(response){
+function addInfo(response, flag_error){
+    if(flag_error){
+        for (const err in cantoniInfo.error) {
+            if(response===err){
+                console.log(err);
+                return cantoniInfo.error[err];}
+            
+          }
+          throw new Error('Failed to find error-corrispondece in the cantoni-info file');
+    }
+    
     for (const cantoneName in cantoniInfo.cantoni) {
         const cantone = cantoniInfo.cantoni[cantoneName];
         if (cantone.name && cantone.name === response) {
           return cantone;
         }
       }
-      return null; // If no match is found
+      throw new Error('Failed to find corrispondece in the cantoni-info file');
 }
 
 
